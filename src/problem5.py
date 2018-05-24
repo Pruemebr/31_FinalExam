7ec1202c4f85bb5a150f19c9058c4714b326efb3
pruemebr <pruemebr@rose-hulman.edu>
2018-05-24 14:28:22 -0400

    CSSE 120 Final Problem 5

--- src/problem5.py
+++ src/problem5.py
@@ -2,9 +2,9 @@
 Final exam, problem 5.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Bryce Pruemer.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -136,11 +136,16 @@ def problem5(m, numbers1, numbers2):
       :type [str]
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
-
+    list = []
+    for k in range(len(numbers1)):
+        if numbers1[k]*m == numbers2[k]:
+            list.append(numbers1[k])
+            list.append(numbers2[k])
+            return list
+    return list
 ###############################################################################
 # Our tests use the following to print error messages in red.
 # Do NOT change it.  You do NOT have to do anything with it.
