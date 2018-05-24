f66149a8f580ef9ac18c00657a9f38456f2d88d8
pruemebr <pruemebr@rose-hulman.edu>
2018-05-24 15:49:42 -0400

    CSSE 120 Final Problem 6

--- src/problem6.py
+++ src/problem6.py
@@ -2,8 +2,8 @@
 Final exam, problem 3.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Bryce Pruemer.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -74,6 +74,79 @@ And this one for n=14:
 
     :type n: int
     """
+    backgroup = ''
+    for z in range(n, 0, -1): #Creates the initial row of the backwards group of numbers
+        if z >= 10:
+            z = z - 10
+        backgroup = backgroup + str(z)
+    print(backgroup)
+
+    num = n
+    row = ''
+    val = 2
+    group = '1'
+    starstr = '**'
+    for o in range(n):
+        for k in range(num, 1, -1):
+            row = row + ' ' #After this, you will have one row's worth of spaces
+        num = num - 1 #number of spaces, setting up for next time around
+        row = row + group
+        group = group + str(val) #Adding the next number to group for the next go around
+        if val == 9:
+            val = 0
+        val = val + 1
+        row = row + ' '
+        row = row + starstr
+        starstr = starstr + '*'
+        row = row + ' '
+        row = row + backgroup
+        temp = backgroup
+        backgroup = '' #empties out backgroup to use again
+        sizebg = len(temp)
+        for g in range(1, sizebg):
+            backgroup = backgroup + temp[g]
+        #backgroup should now be reset with the front number missing
+
+        print(row)
+        row = ''
+
+    # leftstr = ''
+    # stars = '**'
+    # holder = []
+    # hvar2 = 0
+    # spacecount = n
+    #
+    # for hvar in range(n):
+    #     holder.append('')
+    #     for var in range(spacecount, 0, -1):
+    #         holder[1].append('')
+    #     spacecount = spacecount - 1
+    #
+    #
+    #
+    # for k in range(1,n+1):
+    #     if k >= 10:
+    #         k = k - 10
+    #     leftstr =leftstr + str(k)
+    #     holder[hvar2]=holder[hvar2] + (leftstr)
+    #     hvar2 = hvar2 + 1
+    #
+    #
+    # for s in range(len(holder)):
+    #     holder[s] = holder[s] + ''
+    #
+    # # for count in range(len(holder)):
+    # #     holder[count] = holder[count] + stars
+    # #     stars = stars + '*'
+    #
+    # for p in range(len(holder)):
+    #     print(holder[p])
+
+
+
+
+
+
     # ------------------------------------------------------------------
     # TODO: Implement and test this function.
     #          Some tests are already written for you (above).
